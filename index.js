4f8f6326ba4fc05d6a1a2db64c4a9119c04dec39
gan <gan131201@gmail.com>
2020-02-09 21:10:07 +0700

    no message

--- index.js
+++ index.js
@@ -17,81 +17,97 @@ const Aftership = require('aftership')('973859e8-9d21-4715-9baa-fd0ccf2fd133');
 app.get('/',(req,res)=>{
     console.log('/')
     res.send(req.body.name)
-    // res.send("123123123")
 });
 app.get('/api/thaipost',(req,res)=>{
-    console.log(req.query.id)
     console.log('/api/:id')
     gettrack(req,req.query.id,res)
-    // DetectTrack(req,'EF582568151TH',res)
 });
+app.get('/api/testflex',(req,res)=>{
+    console.log('/api/testflex')
+    
+    var json = {
+        "type": "bubble",
+        "body": {
+          "type": "box",
+          "layout": "vertical",
+          "contents": [
+            {
+              "type": "text",
+              "text": "Example"
+            }
+          ]
+        }
+      }
+    res.send(json)
+
+})
 app.get('/api/tour',(req,res)=>{
     console.log('/api/tour')
     gettour(req,res)
 });
 
 ///Thai Post
-    async function gettrack(req, message,res)
-    {
-        let promise_token = new Promise(resolve => {
-            var options = {
-                method: 'POST',
-                uri: 'https://trackapi.thailandpost.co.th/post/api/v1/authenticate/token',
-                strictSSL: false,
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': 'Token ' + config.thaipost.token
-                }
-            };
-            request(options, function(error, response, body) {
-                console.log(error)
-                resolve(JSON.parse(body));
-            });
-        });
-        
-        let access_token = await promise_token;
-        let params = {
-            "status": "all",
-            "language": "TH",
-            "barcode": [
-                message
-           ]
+async function gettrack(req, message,res)
+{
+    let promise_token = new Promise(resolve => {
+        var options = {
+            method: 'POST',
+            uri: 'https://trackapi.thailandpost.co.th/post/api/v1/authenticate/token',
+            strictSSL: false,
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Token ' + config.thaipost.token
+            }
         };
-        let promise_track = new Promise(resolve => {
-            var options = {
-                method: 'POST',
-                uri: 'https://trackapi.thailandpost.co.th/post/api/v1/track',
-                strictSSL: false,
-                body: JSON.stringify(params),
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': 'Token ' + access_token.token
-                }
-            };
-            
-            request(options, function(error, response, body) {
-                // console.log("Requested Items")
-                resolve( JSON.parse(body) );
-            });
+        request(options, function(error, response, body) {
+            console.log(error)
+            resolve(JSON.parse(body));
         });
-
-        let tracks = await promise_track;
-        var lastCheckpoint;
+    });
+    
+    let access_token = await promise_token;
+    let params = {
+        "status": "all",
+        "language": "TH",
+        "barcode": [
+            message
+        ]
+    };
+    let promise_track = new Promise(resolve => {
+        var options = {
+            method: 'POST',
+            uri: 'https://trackapi.thailandpost.co.th/post/api/v1/track',
+            strictSSL: false,
+            body: JSON.stringify(params),
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Token ' + access_token.token
+            }
+        };
         
-        tracks.response.items[message].forEach(element => {
-            console.log(element.status_description)
-            lastCheckpoint = element.status_description
+        request(options, function(error, response, body) {
+            // console.log("Requested Items")
+            resolve( JSON.parse(body) );
         });
+    });
 
-        const returnObj = {
-            message : 'สถานะปัจจุบัน : '+lastCheckpoint
-        }
-        if(lastCheckpoint==undefined){
-            returnObj.message = 'ไม่พบพัสดุดังกล่าว'
-        }
-        res.send(returnObj)
+    let tracks = await promise_track;
+    var lastCheckpoint;
+    
+    tracks.response.items[message].forEach(element => {
+        console.log(element.status_description)
+        lastCheckpoint = element.status_description
+    });
 
-        
+    const returnObj = {
+        message : 'สถานะปัจจุบัน : '+lastCheckpoint
+    }
+    if(lastCheckpoint==undefined){
+        returnObj.message = 'ไม่พบพัสดุดังกล่าว'
+    }
+    res.send(returnObj)
+
+    
     }
 ///
     async function DetectTrack(req,code,res){
